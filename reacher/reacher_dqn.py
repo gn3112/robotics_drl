2bba3aaf31f6c2b6a6eddac9a6a8f2c2ced88a68
gn3112 <georgesnomicos@gmail.com>
2019-04-08 17:53:40 +0100

    corrected eval

--- reacher/reacher_dqn.py
+++ reacher/reacher_dqn.py
@@ -73,7 +73,7 @@ class DQN(nn.Module):
         return self.fc1(x.view(x.size(0),-1))
 
 class evaluation(object):
-    def __init__(n_states=5):
+    def __init__(self,n_states=5):
         self.states_eval = []
         self.resize = T.Compose([T.ToPILImage(),
                                  T.Grayscale(num_output_channels=1),
@@ -85,11 +85,11 @@ class evaluation(object):
             img = self.resize(env.render()).unsqueeze(0).to(device)
             self.states_eval.append(img)
 
-    def get_qvalue(policy_net):
+    def get_qvalue(self,policy_net):
         policy_net.eval()
         qvalues = 0
         for _, img in enumerate(self.states_eval):
-            qvalues += policy_net(torch.tensor(img).view(-1,)).max(1)[0]
+            qvalues += policy_net(img).max(1)[0]
 
         return qvalues/len(self.states_eval)
 
@@ -102,7 +102,7 @@ def select_actions(state,eps_start,eps_end,eps_decay,steps_done,policy_net):
     else:
         return torch.tensor([[random.randrange(9)]], dtype=torch.long), eps_threshold
 
-def optimize_model(policy_net,target_net, optimizer, memory, gamma, batch_size, device):
+def optimize_model(policy_net,target_net, optimizer, memory, gamma, batch_size):
     transitions = memory.sample(batch_size)
     state_batch = []
     action_batch = []
@@ -167,9 +167,6 @@ def train(epoch, learning_rate, batch_size, gamma, eps_start, eps_end,
     target_net.load_state_dict(policy_net.state_dict())
     target_net.eval()
 
-    test = eval_policy.get_qvalue(policy_net)
-    print(test)
-    
     optimizer = optim.RMSprop(policy_net.parameters(), lr = learning_rate)
     memory = Replay_Buffer(buffer_size)
 
@@ -182,10 +179,13 @@ def train(epoch, learning_rate, batch_size, gamma, eps_start, eps_end,
     steps_all = []
     rewards_all = []
 
+    target_upd = 0
+    grad_upd = 0
+
     for update in range(policy_update):
         start_time = time.time()
         while True: # Sample transitions
-            if ep > 21 or (ep < 20 and steps%4 == 0):
+            if len(steps_all) > 21 or (len(steps_all) < 20 and steps_ep%4 == 0):
                 action, eps_threshold = select_actions(obs, eps_start, eps_end,
                                                        eps_decay, sum(steps_all), policy_net)
                 action = action.to(device)
@@ -206,13 +206,12 @@ def train(epoch, learning_rate, batch_size, gamma, eps_start, eps_end,
 
             obs = env.render()
             obs = resize(obs).unsqueeze(0).to(device)
-
             if memory_state == 'full':
                 # TODO: Disregard last transition if incomplete or complete it
                 break
 
             if reward == 100:
-                rewards_all_avg.append(rewards_ep/steps_ep)
+                rewards_all.append(rewards_ep/steps_ep)
                 steps_all.append(steps_ep)
                 successes += 1
                 rewards_ep = 0
@@ -221,7 +220,7 @@ def train(epoch, learning_rate, batch_size, gamma, eps_start, eps_end,
                 env.reset_target_position(random_=random_target)
                 env.reset_robot_position(random_=random_link)
 
-            elif steps == 1500:
+            elif steps_ep == max_steps:
                 rewards_all.append(rewards_ep)
                 steps_all.append(steps_ep)
                 rewards_ep = 0
@@ -234,23 +233,33 @@ def train(epoch, learning_rate, batch_size, gamma, eps_start, eps_end,
             #     print('--- Epsilon threshold: ' + str(eps_threshold) + ' Averaged Return: ' + str(reward_avg) + ' ---')
         end_time = time.time()
 
-        logz.log_tabular('Averaged Steps',np.around(np.average(steps_all),decimals=0))
-        logz.log_tabular('Averaged Rewards',np.around(np.average(rewards_all),decimals=2))
-        logz.log_tabular('Policy update',update)
-        logz.log_tabular('Number of episodes',len(steps_all))
-        logz.log_tabular('Sampling time (s)',(end_time-start_time))
-        logz.log_tabular('Epsilon threshold', eps_threshold)
-        logz.dump_tabular()
-
+        n_iter = memory.__len__()//batch_size
         for _ in range(epoch):
-            if epoch == 4: # update target network parameters
+            if epoch == target_update: # update target network parameters
                 target_net.load_state_dict(policy_net.state_dict())
                 target_net.eval()
-            for _ in range(memory.__len__()//batch_size):
-                optimize_model(policy_net, target_net, optimizer, memory, gamma, batch_size,device)
-
+                target_upd += 1
+            for iter in range(n_iter):
+                grad_upd += 1
+                optimize_model(policy_net, target_net, optimizer, memory, gamma, batch_size)
+                if iter % (n_iter//5) == 0 and iter != 0:
+                    qvalue_eval = eval_policy.get_qvalue(policy_net)
+                    logz.log_tabular('Averaged Steps',np.around(np.average(steps_all),decimals=0))
+                    logz.log_tabular('Averaged Rewards',np.around(np.average(rewards_all),decimals=2))
+                    logz.log_tabular('Successes',successes)
+                    logz.log_tabular('Policy update',update)
+                    logz.log_tabular('Number of episodes',len(steps_all))
+                    logz.log_tabular('Sampling time (s)',(end_time-start_time))
+                    logz.log_tabular('Epsilon threshold', eps_threshold)
+                    logz.log_tabular('Gradient update', grad_upd )
+                    logz.log_tabular('Updates target network', target_upd)
+                    logz.log_tabular('Average q-value evaluation', qvalue_eval)
+                    logz.dump_tabular()
+        
         memory = Replay_Buffer(buffer_size)
         logz.save_pytorch_model(policy_net.state_dict())
+        
+
 
 def setup_logger(logdir, locals_):
     # Configure output directory for logging
@@ -270,7 +279,7 @@ def main():
     parser.add_argument('-buffer_size',default=30000,type=int)
     parser.add_argument('-ep','--epoch',default=8,type=int)
     parser.add_argument('-policy_update',default=12,type=int)
-    parser.add_argument('-target_update',default=160,type=int,help='gradient step')
+    parser.add_argument('-target_update',default=4,type=int,help='every n epoch')
     parser.add_argument('-max_steps',default=1200,type=int)
     parser.add_argument('-gamma',default=0.9999,type=float)
     parser.add_argument('-eps_start',default=0.9,type=float)
