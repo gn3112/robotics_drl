eeb719f94fd4913a4ef6e49ba5cc684baea6b6c5
gn3112 <georgesnomicos@gmail.com>
2019-06-06 13:39:10 +0100

    random robot position

--- reacher/env.py
+++ reacher/env.py
@@ -7,7 +7,7 @@ class Env():
     self._env = environment(continuous_control=continuous)
     self.n_step = 0
   def reset(self):
-    self._env.reset_robot_position(random_=False)
+    self._env.reset_robot_position(random_=True)
     self._env.reset_target_position(random_=True)
     state = self._env.get_obs()
     return torch.tensor(state, dtype=torch.float32)
@@ -15,12 +15,6 @@ class Env():
   def step(self, action):
     reward, done = self._env.step_(action.detach())
     state = self._env.get_obs()
-    self.n_step += 1
-    if self.n_step > 60:
-        self.n_step = 0
-        self.reset()
-    elif done:
-        self.n_step = 0
     return torch.tensor(state, dtype=torch.float32), reward, done
 
   def render(self):
