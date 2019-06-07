6f491fb76040a3f1f9f547608a24b21c47aceab6
Georges Nomicos <georgesnomicos@gmail.com>
2019-06-07 11:20:39 +0100

    testing environment

--- reacher/test_function.py
+++ reacher/test_function.py
@@ -13,14 +13,14 @@ img_ = []
 env.reset()
 for i in range(steps):
     action = [(3 * random.random() - 1.5),(3 * random.random() - 1.5)]
-    env.step(action)
+    env.step([1,1])
     img_.append(env.render())
 
     if i%100 == 0:
         env.reset()
         print(env.get_obs())
 
-home = os.path.expanduser('~')  
+home = os.path.expanduser('~')
 log_video_dir = os.path.join(home,'robotics_drl/reacher/','test_data/','episodes_video')
 os.chdir(log_video_dir)
 vid.from_list(img_,1)
