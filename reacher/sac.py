34182089b30f7e4e25c10ee32d97f9b5da7de23e
gn3112 <georgesnomicos@gmail.com>
2019-07-16 20:00:12 +0100

    q filter for bc loss

--- reacher/sac.py
+++ reacher/sac.py
@@ -239,7 +239,11 @@ def train(BATCH_SIZE, DISCOUNT, ENTROPY_WEIGHT, HIDDEN_SIZE, LEARNING_RATE, MAX_
                         if BEHAVIOR_CLONING:
                             actor_action_dem = batch['action'][i]
                             actual_action_dem, _ = actor(batch['state'][i], log_prob=False, deterministic=True)
-                            behavior_loss = torch.cat((behavior_loss,F.mse_loss(actor_action_dem, actual_action_dem.squeeze()).unsqueeze(dim=0)), dim=0)
+
+                            q_value_actor = (critic_1(batch['state'][i], batch['action'][i]) + critic_2(batch['state'][i], batch['action'][i]))/2
+                            q_value_actual = (critic_1(batch['state'][i], actual_action_dem) + critic_2(batch['state'][i], actual_action_dem))/2
+                            if q_value_actor > q_value_actual: # Q Filter
+                                behavior_loss = torch.cat((behavior_loss,F.mse_loss(actor_action_dem, actual_action_dem.squeeze()).unsqueeze(dim=0)), dim=0)
                         priorities[i] += epsilon_d
                     i += 1
                 if not behavior_loss.nelement() == 0:
